c652deb6338449264d9c248df9e20388f0c5abf0
mhsimpson24 <mhst2015@pomona.edu>
2017-08-01 10:29:34 -0400

    Added commands and updated random number generators

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-
 $method = $_SERVER['REQUEST_METHOD'];
-
 // Process only when method is POST
 if($method == "POST"){
 	$requestBody = file_get_contents('php://input');
@@ -11,13 +9,13 @@ if($method == "POST"){
 	
 	$commandNum = rand(2,5);
 	
-	$array = array("a", "b", "c", "d", "e", "f");
+	$array = array("Touch your toes!", "Pat your head!", "Rub your tummy!", "Twirl around!", "Blink your eyes!", "Hop on one foot!", "Touch your nose!", "Clap your hands!", "Raise your hands to the sky!", "Put your hands on your hips!", "Touch your shoulders!", "Give yourself a hug", "Make a funny face!", "Stand on one foot!", "Cover your eyes!", "Jump in the air!", "Do a jumping jack!", "Clap your hands!", "Snap your fingers!", "Touch your ears!");
 	$fruitArray = array("apple", "banana", "strawberry", "blueberry", "orange", "pineapple", "grapefruit", "lemon");
 	$displayTextArray = array("Listen closely. ");
 	$speechTextArray= array("<speak> Listen closely ");
 	
 	for($x = 0; $x < $commandNum; $x++){
-		$randomIndex = rand(0, 5);
+		$randomIndex = rand(0, 19);
 		array_push($displayTextArray, "Google Says... ", $array[$randomIndex], ", ");
 		array_push($speechTextArray, "<break time=\"3s\"/>  Google Says...", $array[$randomIndex]); 
 	}
@@ -25,14 +23,14 @@ if($method == "POST"){
 	switch ($text){
 		case 'hi':
 			$randomIndex = rand(0, 5);
-			array_push($displayTextArray, " ", $array[$randomIndex], ". ");
-			array_push($displayTextArray, "Uh oh... did you ", $array[$randomIndex], "? If you did, you're out. ");
+			$randomCommandIndex = rand(0, 19);
+			array_push($displayTextArray, " ", $array[$randomCommandIndex], ". ");
+			array_push($displayTextArray, "Uh oh... did you ", $array[$randomCommandIndex], "? If you did, you're out. ");
 			array_push($displayTextArray, "Do you want to keep playing? If you do, say ", $fruitArray[$randomIndex], ". To end the game, say stop.");
 			
-			array_push($speechTextArray, " <break time=\"2s\"/> ", $array[$randomIndex], ". ");
-			array_push($speechTextArray, "<break time=\"2s\"/> Uh Oh, <break time=\"1s\"/> did you ...", $array[$randomIndex], "? If you did, <break time=\"1s\"/> you're out.");
+			array_push($speechTextArray, " <break time=\"2s\"/> ", $array[$randomCommandIndex], ". ");
+			array_push($speechTextArray, "<break time=\"2s\"/> Uh Oh, <break time=\"1s\"/> did you ...", $array[$randomCommandIndex], "? If you did, <break time=\"1s\"/> you're out.");
 			array_push($speechTextArray, "<break time=\"2s\"/> Do you want to keep playing? If you do, say ", $fruitArray[$randomIndex], "<break time=\"2s\"/> To end the game, say stop. </speak>");
-
 			$displayText = implode("", $displayTextArray);
 			$speech = implode("<break time=\"1s\"/>", $speechTextArray);
 			break;
@@ -52,6 +50,4 @@ if($method == "POST"){
 else{
 	echo "Method not allowed";
 }
-
-
 ?>
