cae800b12a5fcfc9746c893e43a3e36b7659bca6
aesmaili <arezuesmaili@yahoo.com>
2017-07-31 22:49:28 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -37,11 +37,6 @@ if($method == "POST"){
 			$speech = implode("<break time=\"1s\"/>", $speechTextArray);
 			break;
 			
-		case 'anything':
-			$speech = "Yes, you can type anything here.";
-			$displayText = "Yes, you can type anything here.";
-			break;
-			
 		default:
 			$speech = "Sorry, I didn't get that. Please ask me something else.";
 			$displayText = "Sorry, I didn't get that. Please ask me something else.";
