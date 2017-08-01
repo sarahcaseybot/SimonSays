48636bac4dacf4685bf2f48f07c52d58be9f2330
aesmaili <arezuesmaili@yahoo.com>
2017-07-31 22:31:34 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -13,34 +13,37 @@ if($method == "POST"){
 	
 	$array = array("a", "b", "c", "d", "e", "f");
 	$fruitArray = array("apple", "banana", "strawberry", "blueberry", "orange", "pineapple", "grapefruit", "lemon");
-	$speechTextArray = array("<speak> Listen closely");
+	$displayTextArray = array("listen closely");
 	
 	for($x = 0; $x < $commandNum; $x++){
 	$randomIndex = rand(0, 5);
-		array_push($speechTextArray, "<break time=\"3s\"/> Google Says...", $array[$randomIndex]); 
+		array_push($displayTextArray, "... ... ... ... ... ... ... ... ... ... Google Says...", $array[$randomIndex]); 
 	}
 	
 	switch ($text){
 		case 'hi':
 			$randomIndex = rand(0, 5);
-			array_push($speechArray, "<break time=\"2s\"/> Uh Oh, <break time=\"1s\"/> did you ...", $array[$randomIndex], "? If you did, <break time=\"1s\"/> you're out.");
-			array_push($speechArray, "<break time=\"2s\"/> Do you want to keep playing? If you do, say ", $fruitArray[$randomIndex], "<break time=\"2s\"/> To end the game, say stop. </speak>");
-			
-			$speech = implode("<break time=\"1s\"/>", $speechText);
+			array_push($displayTextArray, "... ... ... ... ... ... ... ...", $array[$randomIndex]);
+			array_push($displayTextArray, "... ... ... ... ... Uh Oh,... ... did you ...", $array[$randomIndex], "? ... ... If you did, ... ... you're out.");
+			array_push($displayTextArray, "... ... ... ... ... ... Do you want to keep playing? If you do, say ", $fruitArray[$randomIndex], "... ... To end the game, say stop.");
+
+			$displayText = implode("... ...", $displayTextArray);
 			break;
 			
 		case 'anything':
 			$speech = "Yes, you can type anything here.";
+			$displayText = "Yes, you can type anything here.";
 			break;
 			
 		default:
 			$speech = "Sorry, I didn't get that. Please ask me something else.";
+			$displayText = "Sorry, I didn't get that. Please ask me something else.";
 			break;	
 		}
 	
 	$response = new \stdClass();
 	$response->speech = $speech;
-	$response->displayText = "nothing";
+	$response->displayText = $displayText;
 	$response->source = "webhook";
 	echo json_encode($response);
 }
