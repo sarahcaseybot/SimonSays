4be50c80cbc6e1af463b8dceaaa3f3269527d302
mhsimpson24 <mhst2015@pomona.edu>
2017-08-01 11:07:15 -0400

    Punctuation fixes

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if($method == "POST"){
 	
 	$commandNum = rand(2,5);
 	
-	$array = array("Touch your toes!", "Pat your head!", "Rub your tummy!", "Twirl around!", "Blink your eyes!", "Hop on one foot!", "Touch your nose!", "Clap your hands!", "Raise your hands to the sky!", "Put your hands on your hips!", "Touch your shoulders!", "Give yourself a hug", "Make a funny face!", "Stand on one foot!", "Cover your eyes!", "Jump in the air!", "Do a jumping jack!", "Clap your hands!", "Snap your fingers!", "Touch your ears!");
+	$array = array("touch your toes", "pat your head", "rub your tummy", "twirl around", "blink your eyes", "hop on one foot", "touch your nose", "clap your hands", "raise your hands to the sky", "put your hands on your hips", "touch your shoulders", "give yourself a hug", "make a funny face", "stand on one foot", "cover your eyes", "jump in the air", "do a jumping jack", "clap your hands", "snap your fingers", "touch your ears");
 	$fruitArray = array("apple", "banana", "strawberry", "blueberry", "orange", "pineapple", "grapefruit", "lemon");
 	$displayTextArray = array("Listen closely. ");
 	$speechTextArray= array("<speak> Listen closely ");
@@ -17,7 +17,7 @@ if($method == "POST"){
 	for($x = 0; $x < $commandNum; $x++){
 		$randomIndex = rand(0, 19);
 		array_push($displayTextArray, "Google Says... ", $array[$randomIndex], ", ");
-		array_push($speechTextArray, "<break time=\"3s\"/>  Google Says...", $array[$randomIndex]); 
+		array_push($speechTextArray, "<break time=\"3s\"/>  Google Says...", $array[$randomIndex], "."); 
 	}
 	
 	switch ($text){
